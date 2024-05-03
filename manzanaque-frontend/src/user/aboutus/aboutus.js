b558c0180f700fd8139bc768ab7ab41f2ec0562b
wahyu hidayat <123527848+Hidate13@users.noreply.github.com>
2024-05-03 14:51:57 +0700

    third commit

--- manzanaque-frontend/src/user/aboutus/aboutus.js
+++ manzanaque-frontend/src/user/aboutus/aboutus.js
@@ -10,11 +10,11 @@ class AboutUsComponent extends React.Component {
 
   render() {
     const slides = [
-      { url: "http://localhost:3000/image-1.jpg", title: "beach" },
-      { url: "http://localhost:3000/image-2.jpg", title: "boat" },
-      { url: "http://localhost:3000/image-3.jpg", title: "forest" },
-      { url: "http://localhost:3000/image-4.jpg", title: "city" },
-      { url: "http://localhost:3000/image-5.jpg", title: "italy" },
+      { url: "./image-1.jpg", title: "beach" },
+      { url: "./image-2.jpg", title: "boat" },
+      { url: "./image-3.jpg", title: "forest" },
+      { url: "./image-4.jpg", title: "city" },
+      { url: "./image-5.jpg", title: "italy" },
     ];
 
     return (
